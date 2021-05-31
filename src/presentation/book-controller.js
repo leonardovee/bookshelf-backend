ccd90c5c3579ebac8b53aa99cf60429b307b6df0
leonardovee <leoveeph@gmail.com>
2021-05-31 20:44:27 -0300

    feat: ensure BookController calls AddBookUseCase with correct values

--- src/presentation/book-controller.js
+++ src/presentation/book-controller.js
@@ -1,10 +1,15 @@
 const { Unauthorized } = require('./helpers/http-helper')
 
 class BookController {
+  constructor (addBookUseCase) {
+    this.addBookUseCase = addBookUseCase
+  }
+
   async post ({ name, author, description }) {
     if (!name) return Unauthorized('Missing param: name')
     if (!author) return Unauthorized('Missing param: author')
     if (!description) return Unauthorized('Missing param: description')
+    await this.addBookUseCase.add({ name, author, description })
   }
 }
 
