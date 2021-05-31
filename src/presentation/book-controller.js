cf0a792803ce0de0ad652157d2c35398407c6c48
leonardovee <leoveeph@gmail.com>
2021-05-31 20:31:16 -0300

    feat: ensure BookController returns 401 if no description is provided

--- src/presentation/book-controller.js
+++ src/presentation/book-controller.js
@@ -1,9 +1,10 @@
 const { Unauthorized } = require('./helpers/http-helper')
 
 class BookController {
-  async post ({ name, author }) {
+  async post ({ name, author, description }) {
     if (!name) return Unauthorized('Missing param: name')
     if (!author) return Unauthorized('Missing param: author')
+    if (!description) return Unauthorized('Missing param: description')
   }
 }
 
