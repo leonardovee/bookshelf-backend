d18abaef7ec91d84ec1b4bd6589095ac82a2e514
leonardovee <leoveeph@gmail.com>
2021-05-31 22:58:46 -0300

    feat: compose create book controller

---
+++ src/main/composers/create-book-controller-composer.js
@@ -0,0 +1,13 @@
+const CreateBookRepository = require('../../infra/db/mongodb/repositories/create-book-repository.js')
+const AddBookUseCase = require('../../domain/usecase/add-book-usecase.js')
+const CreateBookController = require('../../presentation/controllers/create-book-controller.js')
+
+class CreateBookControllerComposer {
+  static compose () {
+    const createBookRepository = new CreateBookRepository()
+    const addBookUseCase = new AddBookUseCase(createBookRepository)
+    return new CreateBookController(addBookUseCase)
+  }
+}
+
+module.exports = CreateBookControllerComposer
