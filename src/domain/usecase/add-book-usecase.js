479be839ecda2f9b0d518d199c6590d4c2727623
leonardovee <leoveeph@gmail.com>
2021-05-31 21:16:20 -0300

    feat: ensure AddBookUseCase calls CreateBookRepository with correct values

--- src/domain/usecase/add-book-usecase.js
+++ src/domain/usecase/add-book-usecase.js
@@ -1,10 +1,15 @@
 const MissingParamError = require('../../utils/errors/missing-param-error')
 
 class AddBookUseCase {
+  constructor (createBookRepository) {
+    this.createBookRepository = createBookRepository
+  }
+
   async add ({ name, author, description }) {
     if (!name) throw new MissingParamError('name')
     if (!author) throw new MissingParamError('author')
     if (!description) throw new MissingParamError('description')
+    await this.createBookRepository.create({ name, author, description })
   }
 }
 
