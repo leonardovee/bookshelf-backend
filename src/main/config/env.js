324468d8f3ebc01de0f6fa97438276e4a7d47958
leonardovee <leoveeph@gmail.com>
2021-05-31 22:56:19 -0300

    feat: create env file

---
+++ src/main/config/env.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://develop-bookshelf-backend:l4XoFNDY4WryTHSm@cluster0.5umt5.mongodb.net/bookshelf?retryWrites=true&w=majority',
+  port: process.env.PORT || 5050
+}
