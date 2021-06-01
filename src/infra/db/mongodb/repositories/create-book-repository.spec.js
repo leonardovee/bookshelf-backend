0a664a7d882c78a4576e735ac7947692b523399f
leonardovee <leoveeph@gmail.com>
2021-05-31 22:04:34 -0300

    test: ensure CreateBookRepository returns a book on success

---
+++ src/infra/db/mongodb/repositories/create-book-repository.spec.js
@@ -0,0 +1,37 @@
+const CreateBookRepository = require('./create-book-repository.js')
+const MongoHelper = require('../helper/mongo-helper.js')
+
+let bookCollection = null
+
+describe('CreateBook Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    bookCollection = await MongoHelper.getCollection('book')
+    await bookCollection.deleteMany({})
+  })
+
+  describe('create()', () => {
+    test('Should return an book on create success', async () => {
+      const sut = new CreateBookRepository()
+
+      const book = await sut.create({
+        name: 'any_name',
+        author: 'any_author',
+        description: 'any_description'
+      })
+
+      expect(book).toBeTruthy()
+      expect(book.id).toBeTruthy()
+      expect(book.name).toBe('any_name')
+      expect(book.author).toBe('any_author')
+      expect(book.description).toBe('any_description')
+    })
+  })
+})
