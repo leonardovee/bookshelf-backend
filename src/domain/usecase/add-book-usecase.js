70d0aabd1a1d0e0f181477abfe5eaa88e0dad160
leonardovee <leoveeph@gmail.com>
2021-05-31 21:10:16 -0300

    feat: ensure AddBookUseCase throws if no description is provided

--- src/domain/usecase/add-book-usecase.js
+++ src/domain/usecase/add-book-usecase.js
@@ -1,9 +1,10 @@
 const MissingParamError = require('../../utils/errors/missing-param-error')
 
 class AddBookUseCase {
-  async add ({ name, author }) {
+  async add ({ name, author, description }) {
     if (!name) throw new MissingParamError('name')
     if (!author) throw new MissingParamError('author')
+    if (!description) throw new MissingParamError('description')
   }
 }
 
