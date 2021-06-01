18720ed6b50b7b7b4bcfc4bc8c36ada8bb96d64e
leonardovee <leoveeph@gmail.com>
2021-05-31 21:30:24 -0300

    refactor: move request creation to a helper method

--- src/domain/usecase/add-book-usecase.spec.js
+++ src/domain/usecase/add-book-usecase.spec.js
@@ -5,6 +5,12 @@ class CreateBookRepositoryStub {
   async create ({ name, author, description }) {}
 }
 
+const makeFakeRequest = () => ({
+  name: 'any_name',
+  author: 'any_author',
+  description: 'any_description'
+})
+
 const makeSut = () => {
   const createBookRepositoryStub = new CreateBookRepositoryStub()
   const sut = new AddBookUseCase(createBookRepositoryStub)
@@ -44,28 +50,16 @@ describe('AddBook Usecase', () => {
       const { sut, createBookRepositoryStub } = makeSut()
       const createSpy = jest.spyOn(createBookRepositoryStub, 'create')
 
-      await sut.add({
-        name: 'any_name',
-        author: 'any_author',
-        description: 'any_description'
-      })
+      await sut.add(makeFakeRequest())
 
-      expect(createSpy).toHaveBeenCalledWith({
-        name: 'any_name',
-        author: 'any_author',
-        description: 'any_description'
-      })
+      expect(createSpy).toHaveBeenCalledWith(makeFakeRequest())
     })
 
     test('Should returns false if CreateBookRepository fails', async () => {
       const { sut, createBookRepositoryStub } = makeSut()
       jest.spyOn(createBookRepositoryStub, 'create').mockReturnValueOnce(null)
 
-      const response = await sut.add({
-        name: 'any_name',
-        author: 'any_author',
-        description: 'any_description'
-      })
+      const response = await sut.add(makeFakeRequest())
 
       expect(response).toBe(false)
     })
@@ -74,11 +68,7 @@ describe('AddBook Usecase', () => {
       const { sut, createBookRepositoryStub } = makeSut()
       jest.spyOn(createBookRepositoryStub, 'create').mockReturnValueOnce('any_row_unique_id')
 
-      const response = await sut.add({
-        name: 'any_name',
-        author: 'any_author',
-        description: 'any_description'
-      })
+      const response = await sut.add(makeFakeRequest())
 
       expect(response).toBe('any_row_unique_id')
     })
@@ -89,11 +79,7 @@ describe('AddBook Usecase', () => {
         new Promise((resolve, reject) => reject(new Error()))
       )
 
-      const promise = sut.add({
-        name: 'any_name',
-        author: 'any_author',
-        description: 'any_description'
-      })
+      const promise = sut.add(makeFakeRequest())
 
       expect(promise).rejects.toThrow(new Error())
     })
