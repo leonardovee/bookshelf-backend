20f2a5d03287af128d03a12a5f26066719bf7a2e
leonardovee <leoveeph@gmail.com>
2021-06-03 10:55:33 -0300

    test: ensure GetBookUseCase calls GetBookRepository with correct values

--- src/domain/usecase/get-book-usecase.spec.js
+++ src/domain/usecase/get-book-usecase.spec.js
@@ -1,10 +1,20 @@
 const MissingParamError = require('../../utils/errors/missing-param-error')
 const GetBookUseCase = require('./get-book-usecase')
 
+class GetBookRepositoryStub {
+  async get ({ _id }) {}
+}
+
+const makeFakeRequest = () => ({
+  _id: 'any_id'
+})
+
 const makeSut = () => {
-  const sut = new GetBookUseCase()
+  const getBookRepositoryStub = new GetBookRepositoryStub()
+  const sut = new GetBookUseCase(getBookRepositoryStub)
   return {
-    sut
+    sut,
+    getBookRepositoryStub
   }
 }
 
@@ -15,5 +25,14 @@ describe('Get Book Usecase', () => {
     const promise = sut.get({})
 
     expect(promise).rejects.toThrow(new MissingParamError('_id'))
+  })
+
+  test('Should call GetBookRepository with correct values', async () => {
+    const { sut, getBookRepositoryStub } = makeSut()
+    const createSpy = jest.spyOn(getBookRepositoryStub, 'get')
+
+    await sut.get(makeFakeRequest())
+
+    expect(createSpy).toHaveBeenCalledWith(makeFakeRequest())
   })
 })
