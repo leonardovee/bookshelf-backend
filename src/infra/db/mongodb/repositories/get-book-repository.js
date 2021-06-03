16989010c3971e777825a945172f3db1351a6671
leonardovee <leoveeph@gmail.com>
2021-06-03 11:49:05 -0300

    feat: find by ObjectId

--- src/infra/db/mongodb/repositories/get-book-repository.js
+++ src/infra/db/mongodb/repositories/get-book-repository.js
@@ -1,9 +1,10 @@
+const { ObjectId } = require('mongodb')
 const MongoHelper = require('../helper/mongo-helper.js')
 
 class GetBookRepository {
   async get ({ _id }) {
     const bookCollection = await MongoHelper.getCollection('books')
-    const result = await bookCollection.findOne({ _id })
+    const result = await bookCollection.findOne({ _id: ObjectId(_id) })
     return MongoHelper.map(result)
   }
 }
