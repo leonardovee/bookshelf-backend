9a452a6edcbd95d5a0faa829a1b5d74718350207
leonardovee <leoveeph@gmail.com>
2021-06-03 10:32:40 -0300

    test: ensure GetBookController calls GetBookUseCase with correct values

--- src/presentation/controllers/get-book-controller.spec.js
+++ src/presentation/controllers/get-book-controller.spec.js
@@ -1,10 +1,22 @@
 const GetBookController = require('./get-book-controller')
 const { Unauthorized } = require('../helpers/http-helper.js')
 
+class GetBookUseCaseStub {
+  async get ({ _id }) {}
+}
+
+const makeFakeRequest = () => ({
+  body: {
+    _id: 'any_id'
+  }
+})
+
 const makeSut = () => {
-  const sut = new GetBookController()
+  const getBookUseCaseStub = new GetBookUseCaseStub()
+  const sut = new GetBookController(getBookUseCaseStub)
   return {
-    sut
+    sut,
+    getBookUseCaseStub
   }
 }
 
@@ -17,5 +29,14 @@ describe('Get Book Controller', () => {
 
     expect(response.body).toBe(error.body)
     expect(response.statusCode).toBe(error.statusCode)
+  })
+
+  test('Should call GetBookUseCase with correct values', async () => {
+    const { sut, getBookUseCaseStub } = makeSut()
+    const addSpy = jest.spyOn(getBookUseCaseStub, 'get')
+
+    await sut.route(makeFakeRequest())
+
+    expect(addSpy).toHaveBeenCalledWith(makeFakeRequest().body)
   })
 })
