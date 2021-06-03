d3de42daa5ee2a7f1fba3e3ef299ef0a98c99b12
leonardovee <leoveeph@gmail.com>
2021-06-03 11:30:47 -0300

    feat: compose GetBookController

---
+++ src/main/composers/get-book-controller-composer.js
@@ -0,0 +1,13 @@
+const GetBookRepository = require('../../infra/db/mongodb/repositories/get-book-repository.js')
+const GetBookUseCase = require('../../domain/usecase/get-book/get-book-usecase.js')
+const GetBookController = require('../../presentation/controllers/get-book/get-book-controller.js')
+
+class GetBookControllerComposer {
+  static compose () {
+    const getBookRepository = new GetBookRepository()
+    const getBookUseCase = new GetBookUseCase(getBookRepository)
+    return new GetBookController(getBookUseCase)
+  }
+}
+
+module.exports = GetBookControllerComposer
