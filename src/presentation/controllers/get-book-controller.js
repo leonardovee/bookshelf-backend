593565c3e858566b8f0727af13fa9f0eec2ab5bf
leonardovee <leoveeph@gmail.com>
2021-06-03 10:32:37 -0300

    feat: ensure GetBookController calls GetBookUseCase with correct values

--- src/presentation/controllers/get-book-controller.js
+++ src/presentation/controllers/get-book-controller.js
@@ -1,9 +1,14 @@
 const { Unauthorized } = require('../helpers/http-helper')
 
 class GetBookController {
+  constructor (getBookUseCase) {
+    this.getBookUseCase = getBookUseCase
+  }
+
   async route (httpRequest) {
     const { _id } = httpRequest.body
     if (!_id) return Unauthorized('Missing param: _id')
+    await this.getBookUseCase.get({ _id })
   }
 }
 
