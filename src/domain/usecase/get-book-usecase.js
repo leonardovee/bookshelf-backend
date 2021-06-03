0eac23881951931352dc6e76d1b02a8f911f5d76
leonardovee <leoveeph@gmail.com>
2021-06-03 10:55:30 -0300

    feat: ensure GetBookUseCase calls GetBookRepository with correct values

--- src/domain/usecase/get-book-usecase.js
+++ src/domain/usecase/get-book-usecase.js
@@ -1,8 +1,13 @@
 const MissingParamError = require('../../utils/errors/missing-param-error')
 
 class GetBookUseCase {
+  constructor (getBookRepository) {
+    this.getBookRepository = getBookRepository
+  }
+
   async get ({ _id }) {
     if (!_id) throw new MissingParamError('_id')
+    await this.getBookRepository.get({ _id })
   }
 }
 
