9180ec8905da1a9246f971396a4457b42314a6b3
leonardovee <leoveeph@gmail.com>
2021-06-03 10:50:39 -0300

    refactor: remove unecessary describe

--- src/domain/usecase/add-book-usecase.spec.js
+++ src/domain/usecase/add-book-usecase.spec.js
@@ -21,67 +21,65 @@ const makeSut = () => {
 }
 
 describe('AddBook Usecase', () => {
-  describe('add()', () => {
-    test('Should throw if no name is provided', () => {
-      const { sut } = makeSut()
+  test('Should throw if no name is provided', () => {
+    const { sut } = makeSut()
 
-      const promise = sut.add({})
+    const promise = sut.add({})
 
-      expect(promise).rejects.toThrow(new MissingParamError('name'))
-    })
+    expect(promise).rejects.toThrow(new MissingParamError('name'))
+  })
 
-    test('Should throw if no author is provided', () => {
-      const { sut } = makeSut()
+  test('Should throw if no author is provided', () => {
+    const { sut } = makeSut()
 
-      const promise = sut.add({ name: 'any_name' })
+    const promise = sut.add({ name: 'any_name' })
 
-      expect(promise).rejects.toThrow(new MissingParamError('author'))
-    })
+    expect(promise).rejects.toThrow(new MissingParamError('author'))
+  })
 
-    test('Should throw if no description is provided', () => {
-      const { sut } = makeSut()
+  test('Should throw if no description is provided', () => {
+    const { sut } = makeSut()
 
-      const promise = sut.add({ name: 'any_name', author: 'any_author' })
+    const promise = sut.add({ name: 'any_name', author: 'any_author' })
 
-      expect(promise).rejects.toThrow(new MissingParamError('description'))
-    })
+    expect(promise).rejects.toThrow(new MissingParamError('description'))
+  })
 
-    test('Should call CreateBookRepository with correct values', async () => {
-      const { sut, createBookRepositoryStub } = makeSut()
-      const createSpy = jest.spyOn(createBookRepositoryStub, 'create')
+  test('Should call CreateBookRepository with correct values', async () => {
+    const { sut, createBookRepositoryStub } = makeSut()
+    const createSpy = jest.spyOn(createBookRepositoryStub, 'create')
 
-      await sut.add(makeFakeRequest())
+    await sut.add(makeFakeRequest())
 
-      expect(createSpy).toHaveBeenCalledWith(makeFakeRequest())
-    })
+    expect(createSpy).toHaveBeenCalledWith(makeFakeRequest())
+  })
 
-    test('Should returns false if CreateBookRepository fails', async () => {
-      const { sut, createBookRepositoryStub } = makeSut()
-      jest.spyOn(createBookRepositoryStub, 'create').mockReturnValueOnce(null)
+  test('Should returns false if CreateBookRepository fails', async () => {
+    const { sut, createBookRepositoryStub } = makeSut()
+    jest.spyOn(createBookRepositoryStub, 'create').mockReturnValueOnce(null)
 
-      const response = await sut.add(makeFakeRequest())
+    const response = await sut.add(makeFakeRequest())
 
-      expect(response).toBe(false)
-    })
+    expect(response).toBe(false)
+  })
 
-    test('Should returns unique id if CreateBookRepository succeeds', async () => {
-      const { sut, createBookRepositoryStub } = makeSut()
-      jest.spyOn(createBookRepositoryStub, 'create').mockReturnValueOnce('any_row_unique_id')
+  test('Should returns unique id if CreateBookRepository succeeds', async () => {
+    const { sut, createBookRepositoryStub } = makeSut()
+    jest.spyOn(createBookRepositoryStub, 'create').mockReturnValueOnce('any_row_unique_id')
 
-      const response = await sut.add(makeFakeRequest())
+    const response = await sut.add(makeFakeRequest())
 
-      expect(response).toBe('any_row_unique_id')
-    })
+    expect(response).toBe('any_row_unique_id')
+  })
 
-    test('Should throw if CreateBookRepository throws', async () => {
-      const { sut, createBookRepositoryStub } = makeSut()
-      jest.spyOn(createBookRepositoryStub, 'create').mockReturnValueOnce(
-        new Promise((resolve, reject) => reject(new Error()))
-      )
+  test('Should throw if CreateBookRepository throws', async () => {
+    const { sut, createBookRepositoryStub } = makeSut()
+    jest.spyOn(createBookRepositoryStub, 'create').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
 
-      const promise = sut.add(makeFakeRequest())
+    const promise = sut.add(makeFakeRequest())
 
-      expect(promise).rejects.toThrow(new Error())
-    })
+    expect(promise).rejects.toThrow(new Error())
   })
 })
