1663d6c660b408059f264c23c1baa12efb7d1999
leonardovee <leoveeph@gmail.com>
2021-06-03 11:21:31 -0300

    test: ensure GetBookRepository returns a book

---
+++ src/infra/db/mongodb/repositories/get-book-repository.spec.js
@@ -0,0 +1,38 @@
+const GetBookRepository = require('./get-book-repository.js')
+const MongoHelper = require('../helper/mongo-helper.js')
+
+let bookCollection = null
+
+describe('Get Book Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    bookCollection = await MongoHelper.getCollection('books')
+    await bookCollection.deleteMany({})
+  })
+
+  describe('get()', () => {
+    test('Should return a book on get success', async () => {
+      const insertedBook = await bookCollection.insertOne({
+        name: 'any_name',
+        author: 'any_author',
+        description: 'any_description'
+      })
+      const sut = new GetBookRepository()
+
+      const book = await sut.get({ _id: insertedBook.ops[0]._id })
+
+      expect(book).toBeTruthy()
+      expect(book.id).toBeTruthy()
+      expect(book.name).toBe('any_name')
+      expect(book.author).toBe('any_author')
+      expect(book.description).toBe('any_description')
+    })
+  })
+})
