e5b9efb38f7357f23b3d3fd3eda9a38b8267a580
leonardovee <leoveeph@gmail.com>
2021-06-05 11:25:29 -0300

    feat: ensure ListBookController returns 500 if ListBookUseCase throws

--- src/presentation/controllers/list-book/list-book-controller.js
+++ src/presentation/controllers/list-book/list-book-controller.js
@@ -1,11 +1,17 @@
+const { ServerError } = require('../../helpers/http-helper.js')
+
 class ListBookController {
   constructor (listBookUseCase) {
     this.listBookUseCase = listBookUseCase
   }
 
   async route (httpRequest) {
-    const { query } = httpRequest
-    await this.listBookUseCase.list(query)
+    try {
+      const { query } = httpRequest
+      await this.listBookUseCase.list(query)
+    } catch (error) {
+      return ServerError()
+    }
   }
 }
 
