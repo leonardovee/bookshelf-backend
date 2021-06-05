1875e1193befe1347f9952f234b55e46d00eb745
leonardovee <leoveeph@gmail.com>
2021-06-05 13:30:22 -0300

    feat: ensure ListBookRepository returns the book list by name query

--- src/infra/db/mongodb/repositories/list-book/list-book-repository.js
+++ src/infra/db/mongodb/repositories/list-book/list-book-repository.js
@@ -1,8 +1,9 @@
 const MongoHelper = require('../../helper/mongo-helper.js')
 
 class ListBookRepository {
-  async list ({ offset }) {
+  async list ({ offset, name }) {
     const bookCollection = await MongoHelper.getCollection('books')
+    if (name) return await bookCollection.find({ name: { $regex: name } }).toArray()
     const result = await bookCollection
       .find({})
       .skip(parseInt(offset))
