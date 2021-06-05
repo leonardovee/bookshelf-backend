9088df2aae70a99a9c6b6cde8ae700b3f3a549d5
leonardovee <leoveeph@gmail.com>
2021-06-05 12:50:41 -0300

    feat: compose route

--- src/main/routes/book-routes.js
+++ src/main/routes/book-routes.js
@@ -1,5 +1,6 @@
 const createBookControllerComposer = require('../composers/create-book-controller-composer.js')
 const GetBookControllerComposer = require('../composers/get-book-controller-composer.js')
+const ListBookControllerComposer = require('../composers/list-book-controller-composer.js')
 const { adapt } = require('../adapters/express-router-adapter.js')
 
 module.exports = router => {
@@ -7,4 +8,6 @@ module.exports = router => {
   router.post('/books', adapt(createBookController))
   const getBookControllerComposer = GetBookControllerComposer.compose()
   router.get('/books/:_id', adapt(getBookControllerComposer))
+  const listBookControllerComposer = ListBookControllerComposer.compose()
+  router.get('/books', adapt(listBookControllerComposer))
 }
--- src/presentation/controllers/list-book/list-book-controller.js
+++ src/presentation/controllers/list-book/list-book-controller.js
@@ -8,7 +8,7 @@ class ListBookController {
   async route (httpRequest) {
     try {
       const { offset } = httpRequest.query
-      if (!parseInt(offset)) return Unauthorized('Incorrect param value: offset')
+      if (offset && !parseInt(offset)) return Unauthorized('Incorrect param value: offset')
       const list = await this.listBookUseCase.list({ offset })
       return Ok(list)
     } catch (error) {
