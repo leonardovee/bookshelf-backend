0c55c5e86e187d09cd4e9041bf3acd701afc8e68
leonardovee <leoveeph@gmail.com>
2021-06-05 12:37:16 -0300

    feat: ensure ListBookRepository returns the list based on the offset

--- src/infra/db/mongodb/repositories/list-book/list-book-repository.js
+++ src/infra/db/mongodb/repositories/list-book/list-book-repository.js
@@ -3,7 +3,11 @@ const MongoHelper = require('../../helper/mongo-helper.js')
 class ListBookRepository {
   async list ({ offset }) {
     const bookCollection = await MongoHelper.getCollection('books')
-    const result = await bookCollection.find({}).toArray()
+    const result = await bookCollection
+      .find({})
+      .skip(parseInt(offset))
+      .limit(10)
+      .toArray()
     return result
   }
 }
