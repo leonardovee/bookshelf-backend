0f5eba1ddfef78381c011befd7283d958e3f8b7b
leonardovee <leoveeph@gmail.com>
2021-06-06 16:52:46 -0300

    chore: config dotenv

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -3,6 +3,10 @@ const app = express()
 const setupApp = require('./setup.js')
 const setupRoutes = require('./routes.js')
 
+const dotenv = require('dotenv-safe')
+
+dotenv.config()
+
 setupApp(app)
 setupRoutes(app)
 
--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://develop-bookshelf-backend:l4XoFNDY4WryTHSm@cluster0.5umt5.mongodb.net/bookshelf?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://<user>:<password>@cluster0.5umt5.mongodb.net/<database>?retryWrites=true&w=majority',
   port: process.env.PORT || 5050
 }
